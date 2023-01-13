64ab9be525def3dc1694470934580b2f760eb73b
Saddam <saddam13@cse.pstu.ac.bd>
2023-01-13 20:46:57 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-white px-10">
+      <section className='min-h-screen'>
+        <nav className='p-10 mb-12 flex justify-between'>
+          <h1 className='text-xl font-burtons'>developedbysh</h1>
+          <ul className='flex items-center'>
+            {/* <li><FaBeer /></li> */}
+            <li>
+              <BsFillMoonStarsFill className='cursor-pointer text-2xl'></BsFillMoonStarsFill>
+            </li>
+            <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a></li>
+          </ul>
+        </nav>
+      </section>
     </div>
   );
 }
